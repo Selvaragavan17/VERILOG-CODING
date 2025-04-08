a1e69373721569c75e408f32870e0d07a6c1aa38
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-04-08 19:04:23 +0530

    Create encoder_8to3.v

---
+++ COMBINATIONAL /DAY 1/encoder_8to3.v
@@ -0,0 +1,49 @@
+//design code
+module encoder_8to3(
+  input [7:0]in,
+  output reg [2:0]out);
+  always@(*)begin
+    case(in)
+      8'b00000001:out=3'b000;
+      8'b00000010:out=3'b001;
+      8'b00000100:out=3'b010;
+      8'b00001000:out=3'b011;
+      8'b00010000:out=3'b100;
+      8'b00100000:out=3'b101;
+      8'b01000000:out=3'b110;
+      8'b10000000:out=3'b111;
+
+      
+      endcase
+     end
+endmodule
+
+
+//testbench
+module encoder_8to3_tb;
+  reg [7:0]in;
+  wire [2:0]out;
+
+  encoder_8to3 uut (in,out);
+
+  initial begin
+    in=8'b00000001;#10;
+    in=8'b00000010;#10;
+    in=8'b00000100;#10;
+    in=8'b00001000;#10;
+    in=8'b00010000;#10;
+    in=8'b00100000;#10;
+    in=8'b01000000;#10;
+    in=8'b10000000;#10;
+    $finish;
+  end
+
+  initial begin
+    $monitor("Time=%0t Input=%b output=%b ", $time,in,out);
+  end
+
+  initial begin
+    $dumpfile("encoder_8to3.vcd");
+    $dumpvars(1, encoder_8to3_tb);
+  end
+endmodule
