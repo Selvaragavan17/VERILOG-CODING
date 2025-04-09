1842dd15952bc37941c426871566a93ef6e79329
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-04-09 19:27:19 +0530

    Create tlatch.v

---
+++ COMBINATIONAL /DAY 2/tlatch.v
@@ -0,0 +1,33 @@
+//Design code
+module tlatch(
+  input en,t,
+   output reg q);
+ always@(*)begin
+   if(en)
+     q=~t;
+  end
+endmodule
+
+//Testbench code
+module tlatch_tb;
+reg en,t;                                                                     
+wire q;                                                                         
+  tlatch uut(.en(en),.t(t),.q(q));                                              
+  initial begin                                                               
+    en=0;#5;                                                                    
+    t=1;#5;                                                                   
+    en=1;#5;                                                                    
+    t=0;#5;                                                                    
+    t=0;#5;                                                                    
+    en=1;#5;
+    t=1;#5;
+    t=0;#5;
+  end                                                                          
+  initial begin                                                               
+    $monitor("time=%0t EN=%b T=%b Q=%b",$time,en,t,q);                          
+  end                                                                          
+  initial begin                                                               
+    $dumpfile("tlatch.vcd");                                                   
+    $dumpvars(1,tlatch_tb);                                                   
+  end                                                                         
+endmodule 
