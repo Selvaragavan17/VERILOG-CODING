909fba8ebedc61027b6170c8a5c4a1b8c0e21055
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-04-10 11:23:33 +0530

    Create t_flipflop.v

---
+++ VERILOG/DAY 3/t_flipflop.v
@@ -0,0 +1,42 @@
+//design code
+module t_flipflop(
+  input clk,rst,t,
+  output reg q);
+  always@(posedge clk)begin
+    if(rst==0)
+      q<=~t;
+    else
+      q<=0;
+  end
+endmodule
+
+//testbench code
+module t_flipflop_tb;
+  reg clk,rst,t;
+  wire q;
+  t_flipflop uut(.clk(clk),.rst(rst),.t(t),.q(q));
+ 
+  initial begin
+    $monitor("Time=%0t | ClK=%b RST=%b T=%b  Q=%b", $time, clk, rst, t, q);
+  end
+  initial begin
+    $dumpfile("t_flipflop.vcd");
+    $dumpvars(1, t_flipflop_tb);
+  end
+  
+  initial begin
+    clk=0;
+    forever #5 clk=~clk;
+  end
+  
+  initial begin
+    rst=0;
+    t=1;#10;
+    t=1;#10;
+    t=0;#10;
+    rst=1;#10;
+    t=1;#10;
+    t=0;#10;
+    rst=1;
+    $finish;
+  end
