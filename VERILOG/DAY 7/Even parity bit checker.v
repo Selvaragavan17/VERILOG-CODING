3b86c15febe9987117b6bd3f38d2931188641ddf
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-01 15:02:35 +0530

    Create Even parity bit checker.v

---
+++ VERILOG/DAY 7/Even parity bit checker.v
@@ -0,0 +1,39 @@
+//design code
+module even_parity_bit_checker(
+  input a,b,c,p,
+  output reg out);
+  
+  always@(*)begin
+    assign out=(a^b^c^p);
+  end
+endmodule
+
+//testbench code
+module even_parity_bit_checker_tb;
+  reg a,b,c,p;
+  wire out;
+  
+  even_parity_bit_checker uut(a,b,c,p,out);
+  
+  initial begin
+    $dumpfile("even_parity_bit_checker.vcd");
+    $dumpvars(1,even_parity_bit_checker_tb);
+  end
+  
+  initial begin
+    a=0; b=0; c=0; p=0;
+	#5a=0; b=0; c=0; p=0;
+	#5a=0; b=0; c=1; p=1;
+	#5a=0; b=1; c=0; p=1;
+	#5a=0; b=1; c=1; p=0;
+	#5a=1; b=0; c=0; p=1;
+	#5a=1; b=0; c=1; p=0;
+	#5a=1; b=1; c=0; p=1;
+	#5a=1; b=1; c=1; p=0;
+  end
+  
+  initial begin
+    $monitor("Time=%0t|A=%b|B=%b|C=%b|P=%b|OUT=%b",$time,a,b,c,p,out);
+  end
+  
+endmodule
