8f452737c18ca3086fb03a0276e442429da31cda
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-01 16:46:34 +0530

    Update decoder_3to8.v

--- VERILOG/DAY 1/decoder_3to8.v
+++ VERILOG/DAY 1/decoder_3to8.v
@@ -64,3 +64,65 @@ module decoder_3to8_tb;
 endmodule
 
  
+//ANOTHER METHOD
+//DESIGN CODE
+module decoder_3to8(
+  input [2:0] in,     // 3-bit input
+  input en,           // enable signal
+  output reg [7:0] out // 8-bit output
+);
+  always @(*) begin
+    if(en) begin
+      case(in)
+        3'b000: out = 8'b0000_0001;
+        3'b001: out = 8'b0000_0010;
+        3'b010: out = 8'b0000_0100;
+        3'b011: out = 8'b0000_1000;
+        3'b100: out = 8'b0001_0000;
+        3'b101: out = 8'b0010_0000;
+        3'b110: out = 8'b0100_0000;
+        3'b111: out = 8'b1000_0000;
+        default: out = 8'b0000_0000;
+      endcase
+    end
+    else begin
+      out = 8'b0000_0000;
+    end
+  end
+endmodule
+
+//TESTBENCH CODE
+module decoder_3to8_tb;
+  reg [2:0] in;
+  reg en;
+  wire [7:0] out;
+
+  decoder_3to8 uut (
+    .in(in),
+    .en(en),
+    .out(out));
+
+  initial begin
+    $dumpfile("decoder_3to8_tb.vcd");
+    $dumpvars(0, decoder_3to8_tb);
+
+    en = 0;
+    in = 3'b000; #10;
+    en = 1;
+    in = 3'b000; #10;
+    in = 3'b001; #10;
+    in = 3'b010; #10;
+    in = 3'b011; #10;
+    in = 3'b100; #10;
+    in = 3'b101; #10;
+    in = 3'b110; #10;
+    in = 3'b111; #10;
+    en = 0; in = 3'b101; #10;
+
+    $finish;
+  end
+
+  initial begin
+    $monitor("Time=%0t | EN=%b | IN=%b | OUT=%b", $time, en, in, out);
+  end
+endmodule
