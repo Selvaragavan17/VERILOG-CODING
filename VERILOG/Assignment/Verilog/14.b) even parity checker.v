8bea76018026eaf75ed1986e34c52d6e7ed0591f
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-05 09:40:53 +0530

    Create 14.b) even parity checker.v

---
+++ VERILOG/Assignment/Verilog/14.b) even parity checker.v
@@ -0,0 +1,31 @@
+//design code
+module even_p_c(
+input a,b,c,p,
+output out);
+assign out=(a^b^c^p);
+endmodule
+
+//testbench code
+module even_p_c_tb;
+reg a,b,c,p;
+wire out;
+
+even_p_c uut(a,b,c,p,out);
+
+initial begin
+$dumpfile("even_p_c.vcd");
+$dumpvars(1,even_p_c_tb);
+end
+
+initial begin
+$monitor("Time=%0t|A=%b|B=%b|C=%b|P=%b|OUT=%b",$time,a,b,c,p,out);
+end
+
+initial begin
+a=0;b=1;c=1;p=0;#10;
+a=1;b=1;c=0;p=1;#10;
+a=0;b=1;c=1;p=0;#10;
+a=1;b=1;c=0;p=1;#10;
+a=1;b=1;c=1;p=1;#10;
+end
+endmodule
