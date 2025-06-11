96f5a78ab181d990160de74c8fe6440f27fae61d
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-11 22:11:02 +0530

    Update Q03.v

--- VERILOG/VERILOG TEST/03/Q03.v
+++ VERILOG/VERILOG TEST/03/Q03.v
@@ -1,3 +1,5 @@
+3.Design configurable full adder using parameter construct (Test it for 8 bit and 16 bit full adder). Use $Strobe for Displaying result on transcript 
+
 //design code
 module full_adder #(parameter N=8)(
   input [N-1:0]a,
