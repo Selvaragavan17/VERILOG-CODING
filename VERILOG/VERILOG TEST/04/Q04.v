5711930960d4ddb02ba7076022cd73afb5df4d24
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-11 22:33:23 +0530

    Update Q04.v

--- VERILOG/VERILOG TEST/04/Q04.v
+++ VERILOG/VERILOG TEST/04/Q04.v
@@ -4,21 +4,43 @@
   then continue displaying the results. Note: |B|C means (not B) and (not c) 
 
 //design code
-module gates(
-  input a,b,c,d,e,
-  output reg y);
-  
-  wire o1,o2,o3,o4;
-  wire nb,nc,nd;
-  
-  not #4 b1(nb,b);
-  not #4 b2(nc,b);
-  not #4 b3(nb,b);
-  
-  and #3 a1(o1,a,nb,nc);
-  and #3 a2(o2,a,b,e);
-  and #3 a3(o3,nb,c);
-  and #3 a4(o4,c,nd);
-  
-  or  #2 c1(y,o1,o2,o3,o4);
-endmodule 
+`timescale 1ns / 1ps
+
+module logic_expression(
+  input A, B, C, D, E,
+  output Y
+);
+
+  // Internal wires
+  wire nB, nC, nD;
+  wire and1, and2, and3, and4;
+  wire temp1, temp2;
+  wire or1, or2;
+
+  // NOT gates with delay 4
+  not #4 inv1(nB, B);
+  not #4 inv2(nC, C);
+  not #4 inv3(nD, D);
+
+  // AND gate: A & ~B
+  and #3 a1(temp1, A, nB);
+  // AND gate: (A & ~B) & ~C
+  and #3 a2(and1, temp1, nC);
+
+  // AND gate: A & B
+  and #3 a3(temp2, A, B);
+  // AND gate: (A & B) & E
+  and #3 a4(and2, temp2, E);
+
+  // AND gate: ~B & ~C
+  and #3 a5(and3, nB, nC);
+
+  // AND gate: C & ~D
+  and #3 a6(and4, C, nD);
+
+  // OR gates combining all
+  or #2 o1(or1, and1, and2);
+  or #2 o2(or2, and3, and4);
+  or #2 o3(Y, or1, or2);
+
+endmodule
