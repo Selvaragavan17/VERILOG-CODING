d2dafcb8ac03432af2715a3d1841dd8f0e1b623e
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-12 18:11:08 +0530

    Update Q05_tb.v

--- VERILOG/VERILOG TEST/05/Q05_tb.v
+++ VERILOG/VERILOG TEST/05/Q05_tb.v
@@ -2,29 +2,29 @@
 `timescale 1ns/1ps
 
 module tb_clk_divider;
-    reg clk_50MHz;
+    reg clk_MHz;
     reg reset;
-    wire clk_100Hz;
+    wire clk_Hz;
 
 
-    clk_divider_100Hz uut (.clk_50MHz(clk_50MHz),.reset(reset),.clk_100Hz(clk_100Hz));
+    clk_divider_Hz uut (.clk_MHz(clk_MHz),.reset(reset),.clk_Hz(clk_Hz));
 
 
     initial begin
-        clk_50MHz = 0;
-        forever #10 clk_50MHz = ~clk_50MHz; 
+        clk_MHz = 0;
+        forever #10 clk_MHz = ~clk_MHz; 
     end
 
     initial begin
         reset = 1;
         #100 reset = 0;
-        $monitor("Time = %0t ns: clk_100Hz = %b", $time, clk_100Hz);
+        $monitor("Time = %0t ns: clk_Hz = %b", $time, clk_Hz);
         #10000000 $finish; 
     end
 endmodule
 
 //output
-Time = 100000 ns: clk_100Hz = 0
-Time = 5000090000 ns: clk_100Hz = 1
-Time = 10000090000 ns: clk_100Hz = 0
+Time = 100000 ns: clk_Hz = 0
+Time = 5000090000 ns: clk_Hz = 1
+Time = 10000090000 ns: clk_Hz = 0
 testbench.sv:26: $finish called at 10000100000 (1ps)
