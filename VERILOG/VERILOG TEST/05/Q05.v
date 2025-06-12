93841712bf01c8e1f0fc47facc8854f32d3f5acd
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-12 16:28:10 +0530

    Update Q05.v

--- VERILOG/VERILOG TEST/05/Q05.v
+++ VERILOG/VERILOG TEST/05/Q05.v
@@ -1,25 +1,28 @@
 5.Generate a 100Hz clock from a 50MHZ clock in verilog?
 
 //design code
-module clk_display(
-  input clk,
-  input rst,
-  output reg out_clk);
-  reg[19:0]count;
-  
-  always@(posedge clk or posedge rst)begin
-    if(rst) begin
-      count<=19'd0;
-      out_clk<=0;
-    end
-    else begin
-      if(count==19'd249999)begin
-        count<=19'd0;
-        out_clk=~out_clk;
-      end
-      else
-        count<=count+1;
-    end
-  end
-endmodule
+module clk_divider_100Hz (
+    input clk_50MHz,
+    input reset,
+    output reg clk_100Hz
+);
 
+    reg [17:0] counter;
+
+    always @(posedge clk_50MHz or posedge reset) begin
+        if (reset) begin
+            counter <= 0;
+            clk_100Hz <= 0;
+        end
+        else begin
+            if (counter == 249999) begin 
+                counter <= 0;
+                clk_100Hz <= ~clk_100Hz; 
+            end
+            else begin
+                counter <= counter + 1;
+            end
+        end
+    end
+
+endmodule
