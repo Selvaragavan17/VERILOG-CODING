bc484d6119173e399140ef1f827efe74c105a218
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-12 16:12:25 +0530

    Create Q01.v

---
+++ VERILOG/VERILOG TEST/01/Q01.v
@@ -0,0 +1,43 @@
+1 22 333 4444 55555 4444 333 22 1 display this sequence in diamond shape ?
+  /////code//////
+
+module diamond_pattern_tb;
+  integer i, j, k;
+  initial begin
+    // Upper (1 to 5)
+    for (i = 1; i <= 5; i = i + 1) begin
+      
+      for (j = 5; j > i; j = j - 1)          // Print spaces
+        $write(" ");
+     
+      for (k = 1; k <= i; k = k + 1)         // Print digits
+        $write("%0d", i);
+      $display("");  // Newline
+    end
+
+    // Lower (4 to 1)
+    for (i = 4; i >= 1; i = i - 1) begin
+    
+      for (j = 5; j > i; j = j - 1)          // Print spaces
+        $write(" ");
+     
+      for (k = 1; k <= i; k = k + 1)      // Print digits
+        $write("%0d", i);
+      $display("");  // Newline
+    end
+
+    $finish;
+  end
+
+endmodule
+
+  //output//
+    1
+   22
+  333
+ 4444
+55555
+ 4444
+  333
+   22
+    1
