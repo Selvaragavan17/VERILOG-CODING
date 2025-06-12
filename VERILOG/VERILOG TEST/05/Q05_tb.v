613889e7b702701f51ca899d05828aefe066195d
Selvaragavan17 <146157252+Selvaragavan17@users.noreply.github.com>
2025-06-12 16:28:48 +0530

    Update Q05_tb.v

--- VERILOG/VERILOG TEST/05/Q05_tb.v
+++ VERILOG/VERILOG TEST/05/Q05_tb.v
@@ -1,33 +1,30 @@
 //testbench code
-module clk_display_tb;
-  reg clk;
-  reg rst;
-  wire out_clk;
+`timescale 1ns/1ps
+
+module tb_clk_divider;
+    reg clk_50MHz;
+    reg reset;
+    wire clk_100Hz;
 
 
-  clk_display uut (.clk(clk),.rst(rst),.out_clk(out_clk));
+    clk_divider_100Hz uut (.clk_50MHz(clk_50MHz),.reset(reset),.clk_100Hz(clk_100Hz));
 
 
-  initial begin
-    clk = 0;
-    forever #10 clk = ~clk;
-  end
+    initial begin
+        clk_50MHz = 0;
+        forever #10 clk_50MHz = ~clk_50MHz; 
+    end
 
-
-  initial begin
-    $monitor("Time=%0t|clk=%b|rst=%b|out_clk=%b",$time,clk,rst,out_clk);
-  end
-
-  initial begin
-  $dumpfile("clk_display.vcd");
-  $dumpvars(0, clk_display_tb);
-
-  rst = 1; #50;
-  rst = 0;
-
-  #20000000;  
-  $finish;
-end
-
-
+    initial begin
+        reset = 1;
+        #100 reset = 0;
+        $monitor("Time = %0t ns: clk_100Hz = %b", $time, clk_100Hz);
+        #10000000 $finish; 
+    end
 endmodule
+
+//output
+Time = 100000 ns: clk_100Hz = 0
+Time = 5000090000 ns: clk_100Hz = 1
+Time = 10000090000 ns: clk_100Hz = 0
+testbench.sv:26: $finish called at 10000100000 (1ps)
