ce757e573929a74d5d0561495946d1cdf1fb7e9d
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-27 18:25:45 +0530

    Update Selva.v

--- Coding/Selva.v
+++ Coding/Selva.v
@@ -1,59 +1,56 @@
-module half_adder (
-    input  a, b,
-    output sum, carry);
-assign sum   = a ^ b; // XOR
-assign carry = a & b; // AND
+module half_subtractor (
+    input a, b,
+    output diff, borrow);
+    assign diff   = a ^ b;
+    assign borrow = (~a) & b;
 endmodule
-module full_adder (
-input  a, b, cin,
-output sum, carry);
 
-wire s1, c1, c2;
+module full_subtractor (
+input a, b, bin,    
+output diff, bout);
 
-half_adder HA1 (
+wire d1, b1, b2;  
+
+half_subtractor hs1 (
 .a(a),
 .b(b),
-.sum(s1),
-.carry(c1));
+.diff(d1),
+.borrow(b1));
+half_subtractor hs2 (
+.a(d1),
+.b(bin),
+.diff(diff),
+.borrow(b2));
 
-half_adder HA2 (
-.a(s1),
-.b(cin),
-.sum(sum),
-.carry(c2)
-);
-
-or (carry, c1, c2);
+or(bout, b1, b2);
 
 endmodule
 
 
 `timescale 1ns/1ps
-module tb_full_adder;
 
-    reg a, b, cin;
-    wire sum, carry;
+module tb_full_subtractor;
 
-    full_adder dut (
-        .a(a),
-        .b(b),
-        .cin(cin),
-        .sum(sum),
-        .carry(carry));
+reg a, b, bin;
+wire diff, bout;
 
-    initial begin
-        $monitor("time=%0t | a=%b b=%b cin=%b -> sum=%b carry=%b",
-                  $time, a, b, cin, sum, carry);
+full_subtractor dut (
+.a(a), .b(b), .bin(bin),
+.diff(diff), .bout(bout) );
 
-        a=0; b=0; cin=0; #10;
-        a=0; b=0; cin=1; #10;
-        a=0; b=1; cin=0; #10;
-        a=0; b=1; cin=1; #10;
-        a=1; b=0; cin=0; #10;
-        a=1; b=0; cin=1; #10;
-        a=1; b=1; cin=0; #10;
-        a=1; b=1; cin=1; #10;
+initial begin
+$monitor("Time=%0t | a=%b b=%b bin=%b -> diff=%b bout=%b",
+$time, a, b, bin, diff, bout);
 
-        $finish;
-    end
+a=0; b=0; bin=0; #10;
+a=0; b=0; bin=1; #10;
+a=0; b=1; bin=0; #10;
+a=0; b=1; bin=1; #10;
+a=1; b=0; bin=0; #10;
+a=1; b=0; bin=1; #10;
+a=1; b=1; bin=0; #10;
+a=1; b=1; bin=1; #10;
+
+$finish;
+end
 endmodule
