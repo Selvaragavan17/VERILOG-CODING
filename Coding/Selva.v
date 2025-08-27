d10b0f5d2a7398acbc9796e48362808d8a24a5dd
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-27 17:57:25 +0530

    Update Selva.v

--- Coding/Selva.v
+++ Coding/Selva.v
@@ -1,45 +1,59 @@
-`timescale 1ns/1ps
-
 module half_adder (
-input  a, b,
-output reg sum, carry);
+    input  a, b,
+    output sum, carry);
+assign sum   = a ^ b; // XOR
+assign carry = a & b; // AND
+endmodule
+module full_adder (
+input  a, b, cin,
+output sum, carry);
 
-always @(*) begin
-sum   = a ^ b;  // XOR operation
-carry = a & b;  // AND operation
-end
+wire s1, c1, c2;
+
+half_adder HA1 (
+.a(a),
+.b(b),
+.sum(s1),
+.carry(c1));
+
+half_adder HA2 (
+.a(s1),
+.b(cin),
+.sum(sum),
+.carry(c2)
+);
+
+or (carry, c1, c2);
 
 endmodule
 
+
 `timescale 1ns/1ps
+module tb_full_adder;
 
-module tb_half_adder;
+    reg a, b, cin;
+    wire sum, carry;
 
-reg a, b;
-wire sum, carry;
+    full_adder dut (
+        .a(a),
+        .b(b),
+        .cin(cin),
+        .sum(sum),
+        .carry(carry));
 
- 
-half_adder uut (
-.a(a), .b(b),
-.sum(sum), .carry(carry));
+    initial begin
+        $monitor("time=%0t | a=%b b=%b cin=%b -> sum=%b carry=%b",
+                  $time, a, b, cin, sum, carry);
 
-initial begin
-$dumpfile("tb_half_adder.vcd");
-$dumpvars(0, tb_half_adder);
-
-a=0; b=0; #10;
-$display("a=%b b=%b => sum=%b carry=%b (Expected: 0 0)", a, b, sum, carry);
-
-a=0; b=1; #10;
-$display("a=%b b=%b => sum=%b carry=%b (Expected: 1 0)", a, b, sum, carry);
-
-a=1; b=0; #10;
-$display("a=%b b=%b => sum=%b carry=%b (Expected: 1 0)", a, b, sum, carry);
-
-a=1; b=1; #10;
-$display("a=%b b=%b => sum=%b carry=%b (Expected: 0 1)", a, b, sum, carry);
+        a=0; b=0; cin=0; #10;
+        a=0; b=0; cin=1; #10;
+        a=0; b=1; cin=0; #10;
+        a=0; b=1; cin=1; #10;
+        a=1; b=0; cin=0; #10;
+        a=1; b=0; cin=1; #10;
+        a=1; b=1; cin=0; #10;
+        a=1; b=1; cin=1; #10;
 
         $finish;
     end
-
 endmodule
