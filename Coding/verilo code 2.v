3ac7b87c558914701a318c4157cdeaf446bfae45
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-08-28 12:19:56 +0530

    Update verilo code 2.v

--- Coding/verilo code 2.v
+++ Coding/verilo code 2.v
@@ -8,6 +8,23 @@
 | **Execution Time**              | Zero time (purely combinational).                                     | Can consume simulation time.                                         |
 | **Example Use Case**            | Arithmetic calculation, priority encoder logic.                       | Stimulus generation, data transactions, handshake protocols.         |
 
+  module tb;
+  reg a, b;
+
+  initial begin
+    $dumpfile("wave.vcd");  
+    $dumpvars(0, tb);
+
+    a = 0; b = 0;
+    $display("Time=%0t, a=%b, b=%b", $time, a, b);
+    #5 a = 1;
+    #5 b = 1;
+    $monitor("At Time=%0t, a=%b, b=%b", $time, a, b);
+    #10 $finish;
+  end
+endmodule
+
+
 
   | **System Task**   | **Purpose**                     |
 | ----------------- | ------------------------------- |
@@ -35,6 +52,39 @@
 | \`celldefine         | Mark module as a cell                |
 | \`unconnected\_drive | Drive unconnected inputs             |
 | \`resetall           | Reset directives                     |
+
+
+1.Used for Continuous Assignment
+The assign statement is used to drive net data types (like wire) continuously.
+ 2. Can Only Drive Nets, Not Variables
+The LHS of an assign must be a net type (wire, tri, etc.), not a reg.
+3.Implements Combinational Logic
+Commonly used to model combinational logic (AND, OR, XOR, etc.).
+Equivalent to logic gates.
+4.Supports Continuous Driving of Expressions
+5.Can Have Delay (Inertial Delay)
+You can specify delay in an assign statement.
+Delay models the propagation delay of combinational logic.
+  6.Multiple Drivers Allowed on wire
+  7.Cannot Be Used Inside always or initial Blocks
+assign is only for continuous assignment, not procedural assignment.
+  module assign_example;
+  reg a, b, sel;
+  wire y1, y2, y3;
+
+  // Continuous assignments
+  assign y1 = a & b;            // AND gate
+  assign y2 = a | b;            // OR gate
+  assign y3 = (sel) ? a : b;    // 2:1 MUX
+
+  initial begin
+    a = 0; b = 0; sel = 0;
+    #5 a = 1; b = 1;
+    #5 sel = 1;
+    #5 b = 0;
+  end
+endmodule
+
 
 
 
