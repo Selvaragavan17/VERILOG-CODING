6928ab286c6b43118469dcba1a453680fe6111ca
Selvaragavan_K <146157252+Selvaragavan17@users.noreply.github.com>
2025-09-22 11:17:43 +0530

    Update bfm.v

--- hdl verificationn project fifo/bfm.v
+++ hdl verificationn project fifo/bfm.v
@@ -6,7 +6,7 @@ module bfm (
     output reg [7:0] wr_data,         
     output reg wr_enb,          
     output reg rd_enb,          
-    input  [7:0]rd_data,         
+    input  [7:0] rd_data,         
     input fifo_full,
     input fifo_empty,
     input fifo_almost_full,
@@ -15,79 +15,88 @@ module bfm (
     input fifo_underrun
 );
 
+  // Display helper
   task show;
     begin
-      $display("[%0t] rst_n=%0b wr_enb=%0b wr_data=%0h | rd_enb=%0b rd_data=%0h | full=%0b empty=%0b | almost_full=%0d almost_empty=%0d",
-               $time, rst_n, wr_enb, wr_data, rd_enb, rd_data, fifo_full, fifo_empty, fifo_almost_full, fifo_almost_empty);
+      $display("[BFM]---[%0t] rst_n=%0b wr_enb=%0b wr_data=%0h | rd_enb=%0b rd_data=%0h | full=%0b empty=%0b | almost_full=%0b almost_empty=%0b overrun=%0b underrun=%0b",$time, rst_n, wr_enb, wr_data, rd_enb, rd_data,
+fifo_full, fifo_empty, fifo_almost_full, fifo_almost_empty,
+fifo_overrun, fifo_underrun);
     end
   endtask
 
+  // Reset sequence
   task do_reset;
     begin
-      $display("\n[BFM] Reset sequence");
-      rst_n=0;
-      wr_enb=0;
-      rd_enb=0;
-      wr_data=0;
-      repeat (2) @(posedge clk);
+      $display("\n[BFM]---Directed Case 1- Reset sequence");
+      rst_n=0; wr_enb=0; rd_enb=0; wr_data=0;
+      @(posedge clk); show;
       rst_n=1;
-      @(posedge clk); #1; show;
+      @(posedge clk); 
     end
   endtask
 
+  // Simple write
   task do_write(input [7:0] data);
     begin
-      $display("\n[BFM] Writing data = %0h", data);
+      $display("\n[BFM]---Directed Case 2 – Simple Write");
       wr_enb=1; wr_data=data;
       @(posedge clk); #1; show;
       wr_enb=0;
-      @(posedge clk); #1; show;
+      @(posedge clk); 
     end
   endtask
 
+  // Simple read
   task do_read(output [7:0] data);
     begin
-      $display("\n[BFM] Reading data");
+      $display("\n[BFM]---Directed Case 3 – Simple Read");
       rd_enb=1;
       @(posedge clk); #1;show;
       data=rd_data;
       rd_enb=0;
-      @(posedge clk); #1; show;
-      $display("[BFM] Got back = %0h", data);
+      @(posedge clk); 
     end
   endtask
 
-  task do_full_write;
+ task do_full_write;
     integer i;
     begin
-      $display("\n[BFM] Filling FIFO completely");
+      $display("\n[BFM]--- Directed Case 4 - Filling FIFO completely");
       for (i =0; i<8; i=i+1) begin
         wr_enb=1; wr_data=i+8'h10;
         @(posedge clk); #1; show;
       end
       wr_enb=0;
-      @(posedge clk); #1; show;
+      @(posedge clk); 
     end
   endtask
 
   task do_full_read;
     integer i;
     begin
-      $display("\n[BFM] Draining FIFO completely");
+      $display("\n[BFM]--- Directed Case 5 - Draining FIFO completely");
       for (i=0; i<8; i=i+1) begin
         rd_enb=1;
         @(posedge clk); #1; show;
       end
       rd_enb=0;
-      @(posedge clk); #1; show;
+      @(posedge clk); 
     end
   endtask
 
 endmodule
+
 -----------------------------------------------------------------------------------------------------
+// Code your testbench here
+// or browse Examples
+// Code your testbench here
+// or browse Examples
+// Code your testbench here
+// or browse Examples
  `timescale 1ns/1ps
 `include"bfm.sv"
 `include"monitor.sv"
+`include "checker.sv"
 
 module tb_top;
 
@@ -97,6 +106,8 @@ module tb_top;
   wire wr_enb, rd_enb;
   wire [7:0] rd_data;
   wire fifo_full, fifo_empty, fifo_almost_full, fifo_almost_empty, fifo_overrun, fifo_underrun;
+  reg[7:0]d;
+  
 
   // clock generator
   initial clk = 0;
@@ -134,6 +145,7 @@ module tb_top;
     .fifo_underrun(fifo_underrun)
   );
     // MONITOR instance
+  
   monitor mon_inst (
     .clk(clk),
     .rst_n(rst_n),
@@ -148,22 +160,33 @@ module tb_top;
     .fifo_overrun(fifo_overrun),
     .fifo_underrun(fifo_underrun)
   );
+  
+  fifo_checker chk_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .wr_enb(wr_enb),
+    .wr_data(wr_data),
+    .rd_enb(rd_enb),
+    .rd_data(rd_data),
+    .fifo_full(fifo_full),
+    .fifo_empty(fifo_empty),
+    .fifo_overrun(fifo_overrun),
+    .fifo_underrun(fifo_underrun)
+  );
 
 initial begin
-  reg [7:0] d;
-
-  bfm_inst.do_reset();
-  bfm_inst.do_write(8'hA5);
+  bfm_inst.do_reset;
+  bfm_inst.do_write(8'hAA);
   bfm_inst.do_read(d);
-  bfm_inst.do_full_write();
-  bfm_inst.do_full_read();
-
-  #20 $finish;
+  bfm_inst.do_full_write;
+  bfm_inst.do_full_read;
+  $finish;
 end
 
 
 
 endmodule
+
 
 ------------------------------------------------------------------------------------------
 
